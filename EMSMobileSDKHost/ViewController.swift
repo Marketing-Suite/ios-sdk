5527adbeb9cc21621738dc78cd837e36f1fbc756
Mark Dennis Diwa <mark.diwa@cheetahdigital.com>
2020-01-07 15:21:33 +0800

    Add SDK host app

---
+++ EMSMobileSDKHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMSMobileSDKHost
+//
+//  Created by Mark Dennis Diwa on 07/01/2020.
+//  Copyright © 2020 Experian Marketing Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
