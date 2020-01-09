3b63a0fee26c3d1c9bb5273e0bbb502d7143f204
Julius Vitug <julius.vitug@cheetahdigital.com>
2020-01-09 16:33:53 +0800

    Change background url session configuration to default

--- EMSMobileSDK/EMSAPI.swift
+++ EMSMobileSDK/EMSAPI.swift
@@ -12,12 +12,10 @@ import Foundation
 @objc
 public class EMSAPI: NSObject {
     // fields
-    public var backgroundSession: Alamofire.SessionManager
+    public var session: Alamofire.SessionManager
     
     override public init() {
-        //Configure SessionManager
-        let configuration = URLSessionConfiguration.background(withIdentifier: "com.experian.emsmobilesdk")
-        backgroundSession = Alamofire.SessionManager(configuration: configuration)
+        session = Alamofire.SessionManager(configuration: URLSessionConfiguration.default)
         super.init()
     }
     
@@ -30,9 +28,9 @@ public class EMSAPI: NSObject {
         
         let body: Parameters = ["DeviceToken": deviceToken]
         
-        let request = backgroundSession.request(url,
-                                                method: .post,
-                                                parameters: body)
+        let request = session.request(url,
+                                      method: .post,
+                                      parameters: body)
         request.validate().responseJSON(completionHandler: completionHandler)
     }
     
@@ -56,9 +54,9 @@ public class EMSAPI: NSObject {
         
         let body: Parameters = ["DeviceToken": deviceToken]
         
-        let request = backgroundSession.request(url,
-                                                method: .put,
-                                                parameters: body)
+        let request = session.request(url,
+                                      method: .put,
+                                      parameters: body)
         request.validate().responseJSON(completionHandler: completionHandler)
     }
     
@@ -71,9 +69,9 @@ public class EMSAPI: NSObject {
         
         let body: Parameters = ["DeviceToken": deviceToken]
         
-        let request = backgroundSession.request(url,
-                                                method: .delete,
-                                                parameters: body)
+        let request = session.request(url,
+                                      method: .delete,
+                                      parameters: body)
         request.validate().responseJSON(completionHandler: completionHandler)
     }
     
@@ -83,23 +81,23 @@ public class EMSAPI: NSObject {
                         data: Parameters?,
                         completionHandler: @escaping (DataResponse<Any>) -> Void) {
         let urlString: String = "\(region.ats)/ats/post.aspx?cr=\(customerID)&fm=\(formId)"
-        let request = backgroundSession.request(urlString,
-                                                method: .post,
-                                                parameters: data)
+        let request = session.request(urlString,
+                                      method: .post,
+                                      parameters: data)
         request.validate().responseJSON(completionHandler: completionHandler)
     }
     
     public func logDeepLink(_ deepLinkUrl: String,
                             completionHandler: @escaping (DownloadResponse<String>) -> Void) {
-        let request = backgroundSession.download(deepLinkUrl)
+        let request = session.download(deepLinkUrl)
         request.validate().responseString(completionHandler: completionHandler)
     }
     
     public func logEMSOpen(url: String,
                            completionHandler: @escaping (DataResponse<Any>) -> Void) {
-        let request = backgroundSession.request(url,
-                                                method: .get,
-                                                parameters: nil)
+        let request = session.request(url,
+                                      method: .get,
+                                      parameters: nil)
         request.validate().responseJSON(completionHandler: completionHandler)
     }
 }
